6fba37f72e08183e8c1009ea179bf1847a9fb881
Andrew Shell <andrew@andrewshell.org>
2024-12-11 07:44:52 -0600

    mobile not vertically centered

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import CatFactDisplay from './components/CatFactDisplay';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+    <main className="flex min-h-screen flex-col items-center md:justify-center p-4">
       <h1 className="text-7xl text-center font-bold mb-8">Dubious Cat Facts</h1>
       <CatFactDisplay />
       <footer className="fixed bottom-2 right-2">
